ac27cfb98e6012fd3421e56ae1dfb296d81fc718
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-26 00:26:14 +0300

    Redesign Header. Fix img. Relative text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__container">
+        <div className="quotation__icon-container">
+          <img className="quotation__icon" src={icon} alt=""></img>
+        </div>
         <div className="header__text-container">
           <div className="header__quotation">
-            <div className="quotation__icon-container">
-              <img className="quotation__icon" src={icon} alt=""></img>
-            </div>
             <h1 className="header__title">
               "Изучать <span className="memories">воспоминания</span> -{" "}
               <br></br>
