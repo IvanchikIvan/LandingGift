d963b544ba44b71f90504265dee92b78df28a534
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-26 00:17:57 +0300

    Redesign header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,25 @@
 import "./Header.css";
+import icon from "../../Assets/chat-avatar.jpg";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__container">
         <div className="header__text-container">
-          <h1 className="header__title">
-            Изучать воспоминания - <br></br>значит найти лучший подарок
-          </h1>
-          <h2 className="header__subtitle">Эта фраза у меня сформулировалась у меня во время очередного просмотра приятный фотографий нашего, пятого города</h2>
+          <div className="header__quotation">
+            <div className="quotation__icon-container">
+              <img className="quotation__icon" src={icon} alt=""></img>
+            </div>
+            <h1 className="header__title">
+              "Изучать <span className="memories">воспоминания</span> -{" "}
+              <br></br>
+              значит найти лучший <span className="gift">подарок</span>"
+            </h1>
+          </div>
+          <h2 className="header__subtitle">
+            Фраза сформирована во время очередного просмотра фотографик нашего,
+            пятого города
+          </h2>
         </div>
       </div>
     </header>
