c56c1cb3188b381c543314d991ae2890e64f4db8
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-26 07:30:37 +0300

    Rewritten class names

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,9 +11,9 @@ const Header = () => {
         <div className="header__text-container">
           <div className="header__quotation">
             <h1 className="header__title">
-              "Изучать <span className="memories">воспоминания</span> -{" "}
+              "Изучать <span className="title__memories">воспоминания</span> -{" "}
               <br></br>
-              значит найти лучший <span className="gift">подарок</span>"
+              значит найти лучший <span className="title__gift">подарок</span>"
             </h1>
           </div>
           <h2 className="header__subtitle">
