d34339c7654352366f161287707781fafb3ab092
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-29 00:33:57 +0300

    Redesign IWGTC block

--- src/components/i-was-glad-to-come/i-was-glad-to-come.js
+++ src/components/i-was-glad-to-come/i-was-glad-to-come.js
@@ -1,5 +1,6 @@
 import "./i-was-glad-to-come.css";
 import together from "../../Assets/together.jpg";
+import together_2 from "../../Assets/together_2.jpg";
 
 const IWGTC = () => {
   return (
@@ -15,9 +16,23 @@ const IWGTC = () => {
                 <h2>Прибытие в тверь</h2>
                 <p>
                   Я был бесконечно рад приехать к вам. Самые тёплые чувства,
-                  которые я испытал с вами в лагере снова меня достанут
+                  которые я испытал с вами в лагере снова меня посетили
                 </p>
               </div>
+            </div>
+          </div>
+          <div className="i-was-glad-to-come__row">
+            <div className="i-was-glad-to-come-row__text-container">
+              <div className="i-was-glad-to-come-row__text">
+                <h2>Прибытие в тверь</h2>
+                <p>
+                  Я был бесконечно рад приехать к вам. Самые тёплые чувства,
+                  которые я испытал с вами в лагере снова меня посетили
+                </p>
+              </div>
+            </div>
+            <div className="i-was-glad-to-come-row__icon">
+              <img alt="" src={together_2}></img>
             </div>
           </div>
         </div>
