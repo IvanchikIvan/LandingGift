933d8eeae106a0072e6ce2f44b389410db51c595
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-29 22:02:24 +0300

    Created new thank-you__list-items

--- src/components/thank_you/thank-you.js
+++ src/components/thank_you/thank-you.js
@@ -10,11 +10,14 @@ const Thankyou = () => {
             <h1 className="thank-you__title-text">Спасибо за...</h1>
           </div>
           <div className="thank-you__list">
-            <div className="thank-you__list-item"><p>A</p></div>
-            <div className="thank-you__list-item"><p>B</p></div>
-            <div className="thank-you__list-item"><p>C</p></div>
-            <div className="thank-you__list-item"><p>D</p></div>
-            <div className="thank-you__list-item"><p>F</p></div>
+            <div className="thank-you__list-item"><p>Первый разговор в лагере</p></div>
+            <div className="thank-you__list-item"><p>Возможность поспать в два часа ночи на твоей кровати...</p></div>
+            <div className="thank-you__list-item"><p>Возможность посушиться лучшим феном BaByliss Pro</p></div>
+            <div className="thank-you__list-item"><p>Прекрасные выступления</p></div>
+            <div className="thank-you__list-item"><p>Презентацию Твери</p></div>
+            <div className="thank-you__list-item"><p>Твою поддержку</p></div>
+            <div className="thank-you__list-item"><p>Твою доброту</p></div>
+            <div className="thank-you__list-item"><p>Наши общие воспоминания</p></div>
           </div>
         </div>
       </div>
